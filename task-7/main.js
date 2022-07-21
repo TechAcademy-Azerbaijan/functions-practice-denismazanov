d30a328d56923ac5d800153966895690c1b4452b
denismazanov <82777275+denismazanov@users.noreply.github.com>
2022-07-21 23:34:47 +0300

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -1,9 +1,11 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+function kv(num){
+  let i = 1;
+  while(true){
+      if(i*i>num){
+        break
+      }
+      console.log(i*i)
+    i++
+  }
+}
+kv(20)
