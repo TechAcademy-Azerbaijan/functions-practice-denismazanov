f1d6ba6fa18dac4a97369670e020d90a9017abf3
denismazanov <82777275+denismazanov@users.noreply.github.com>
2022-07-21 23:31:25 +0300

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,9 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+function cost(num){
+  let a = num - parseInt(num%100);
+  let b = num%100 - (num%10);
+  let c = num % 10;
+  let result  = a*1 + b*1.5 + c*2;
+  return result
+}
+let a = 245;
+console.log(cost(a))
