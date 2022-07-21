1ca6fbb6684098c76c9dfc754f8a564dcf874c9f
denismazanov <82777275+denismazanov@users.noreply.github.com>
2022-07-21 23:32:52 +0300

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,12 @@
-const prompt = require('prompt');
-prompt.start();
+function arr(array){
+  let list = [];
+  for(let i = 0;i<array.length;i++){
+    list.push(array[array.length -1- i])
+  }
+  return list
+}
+let m =[92846,96841,33582,25998];
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+for(let k = 0;k<arr(m).length;k++){
+  console.log(arr(m)[k])
+}
