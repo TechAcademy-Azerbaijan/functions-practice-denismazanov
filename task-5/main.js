d29f9b0a787fa92fc89d5bfc17c436400f33933e
denismazanov <82777275+denismazanov@users.noreply.github.com>
2022-07-21 23:33:55 +0300

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,12 @@
-const prompt = require('prompt');
-prompt.start();
+function max(array){
+  let m = array[0]
+  for(let i = 0;i<array.length;i++){
+    if(array[i]>m){
+      m = array[i]
+    }
+  }
+  return m
+}
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+array = [4,2,3,1,7,6,5]
+console.log(max(array))
