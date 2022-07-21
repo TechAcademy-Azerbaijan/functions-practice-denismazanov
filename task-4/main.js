3325fd44f35b042ab6bde443935e93bd01ca5838
denismazanov <82777275+denismazanov@users.noreply.github.com>
2022-07-21 23:33:27 +0300

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,11 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+function fibonacci(num){
+  let a = 0; // 0 1 2 3 4 5 6  7 8 
+  let b = 1; // 0 1 1 2 3 5 8 13 21
+  for(let i = 0;i<num;i++){
+    let c = a+b;
+    a = b;
+    b = c; 
+  }
+  return a
+}
+console.log(fibonacci(6))
